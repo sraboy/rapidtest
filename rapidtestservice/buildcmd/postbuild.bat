9a87017f460caed129568dff72e104d0dd2114a9
Steve Lavoie <steve.l@digitaloperatives.com>
2018-02-04 15:04:45 -0600

    Conditional install for service

--- rapidtestservice/buildcmd/postbuild.bat
+++ rapidtestservice/buildcmd/postbuild.bat
@@ -1,12 +1,14 @@
 echo Running postbuild commands...
 @echo off
 
-REM Called as: call PostBuild.bat "$(TargetPath)" "$(ProjectName)"
+REM Called as: call PostBuild.bat "$(TargetPath)" "$(ProjectName)" [install|noinstall]
 
 REM If you're not running VS as administrator, this will fail.
 
 set targetpath=%~1
 set projectname=%~2
+
+if "%~3"=="noinstall" goto :eof
 
 echo Creating service: %projectname%
 sc create  %projectname% binPath= "%targetpath%"
